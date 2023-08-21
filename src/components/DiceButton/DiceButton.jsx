cc2082156f08acbf52131aeb7378a151360ea7d1
Morten010 <97233551+Morten010@users.noreply.github.com>
2023-08-21 11:12:20 +0200

    updated terning

--- src/components/DiceButton/DiceButton.jsx
+++ src/components/DiceButton/DiceButton.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import {BsFillDice1Fill, BsFillDice2Fill} from "react-icons/bs"
+import {BsFillDice1Fill, BsFillDice2Fill, BsFillDice3Fill, BsFillDice4Fill, BsFillDice5Fill, BsFillDice6Fill} from "react-icons/bs"
 
 export default function DiceButton({score, eyes, KastTerning}) {
+
+    
+    const getEyes = () => {
+        switch(eyes) {
+            case 1: 
+                return <BsFillDice1Fill />
+            case 2:
+                return <BsFillDice2Fill />
+            case 3:
+                return <BsFillDice3Fill />
+            case 4:
+                return <BsFillDice4Fill />
+            case 5:
+                return <BsFillDice5Fill />
+            case 6:
+                return <BsFillDice6Fill />
+
+        }
+    }
+
   return (
     <div>
         <p>Score: {score}</p>
         <button
         onClick={() => KastTerning()}
         >
-            {eyes}
+            {getEyes()}
         </button>
     </div>
   )
