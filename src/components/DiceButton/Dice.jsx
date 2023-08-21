d293b17c2d4edbbf956e4cd036e9ab1732cb09b3
Morten010 <97233551+Morten010@users.noreply.github.com>
2023-08-21 12:02:57 +0200

    updated dice

---
+++ src/components/DiceButton/Dice.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BsFillDice1Fill, BsFillDice2Fill, BsFillDice3Fill, BsFillDice4Fill, BsFillDice5Fill, BsFillDice6Fill} from "react-icons/bs"
+
+export default function Dice({eyes}) {
+    switch(eyes) {
+        case 1: 
+            return <BsFillDice1Fill />
+        case 2:
+            return <BsFillDice2Fill />
+        case 3:
+            return <BsFillDice3Fill />
+        case 4:
+            return <BsFillDice4Fill />
+        case 5:
+            return <BsFillDice5Fill />
+        case 6:
+            return <BsFillDice6Fill />
+    }
+}
--- src/components/DiceButton/DiceButton.jsx
+++ src/components/DiceButton/DiceButton.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
-import {BsFillDice1Fill, BsFillDice2Fill, BsFillDice3Fill, BsFillDice4Fill, BsFillDice5Fill, BsFillDice6Fill} from "react-icons/bs"
+import Dice from './Dice'
 
 export default function DiceButton({score, eyes, KastTerning}) {
-
-    
-    const getEyes = () => {
-        switch(eyes) {
-            case 1: 
-                return <BsFillDice1Fill />
-            case 2:
-                return <BsFillDice2Fill />
-            case 3:
-                return <BsFillDice3Fill />
-            case 4:
-                return <BsFillDice4Fill />
-            case 5:
-                return <BsFillDice5Fill />
-            case 6:
-                return <BsFillDice6Fill />
-
-        }
-    }
 
   return (
     <div>
@@ -28,7 +9,7 @@ export default function DiceButton({score, eyes, KastTerning}) {
         <button
         onClick={() => KastTerning()}
         >
-            {getEyes()}
+            <Dice eyes={eyes} />
         </button>
     </div>
   )
