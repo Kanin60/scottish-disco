696d4733c6cdcd295bdbae0e472f1e025abab014
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-08-21 11:04:50 +0200

    Bruger defineret i Bruger-component

---
+++ src/components/Bruger/Bruger.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export const Bruger = () => {
+    const [playerOne, setPlayerOne] = useState(0)
+    const [playerTwo, setPlayerTwo] = useState(0)
+    const [lastPlayer, setLastPlayer] = useState(false)
+}
